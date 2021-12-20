212409d15fbc4c7714e1faeadabe3a4fe873bece
Vitaly Lobatsevich <vitaly.lobatsevich@gmail.com>
2021-12-20 18:52:48 +0300

    Project base

---
+++ src/test/java/by/vitalylobatsevich/courser/CourserApplicationTests.java
@@ -0,0 +1,13 @@
+package by.vitalylobatsevich.courser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
